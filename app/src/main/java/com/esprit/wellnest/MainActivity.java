f5d033e913d1d0afd3131a55d1c888bb39d29465
Achref <achref.maarfi0@gmail.com>
2024-11-07 14:52:01 +0100

    init project structure

---
+++ app/src/main/java/com/esprit/wellnest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.esprit.wellnest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+}
