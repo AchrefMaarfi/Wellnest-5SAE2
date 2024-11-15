669aed1ba19c25047fc60f71ebf4c0e1e15f07b9
Achref <achref.maarfi0@gmail.com>
2024-11-15 09:11:54 +0100

    reclamation ajouter + list

--- app/src/main/java/com/esprit/wellnest/model/Reclamation.java
+++ app/src/main/java/com/esprit/wellnest/model/Reclamation.java
@@ -12,4 +12,18 @@ public class Reclamation {
     @ColumnInfo(name = "reclamation_id")
     @PrimaryKey(autoGenerate = true)
     private int id;
+
+    private String email;
+
+    @ColumnInfo(name = "sujet")
+    private String sujet;
+
+    @ColumnInfo(name = "categorie")
+    private String categorie;
+
+    @ColumnInfo(name = "description")
+    private String description;
+
+    @ColumnInfo(name = "image")
+    private String image;
 }
